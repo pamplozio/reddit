c356004bf99d93372a35a27086dc7f7e49035243
Pamplozio <giusepperosso@hotmail.com>
2023-07-04 16:04:25 +0100

    add route for home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <h1>3 ore a Palermo</h1>
+      <Router>
+        <NavBar/>
+        <h1>3 ore a Palermo</h1>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+        </Routes>
+      </Router>  
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>coRo</div>
+  )
+}
+
+export default Home

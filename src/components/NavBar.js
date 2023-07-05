a2a2e62a9e8aa3ce48797e7f445c48838ca8c9d9
Pamplozio <giusepperosso@hotmail.com>
2023-07-05 16:47:43 +0100

    icon for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from "../assets/coRo logo.png";
-
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
+ 
 function NavBar() {
   return (
     <div className='navbar'>
         <div className='leftSide'>
             <img src={Logo} />
         </div>
-        <div className='rightSide'></div>
+        <div className='hiddenLinks'>
+            <Link to='/'>Home</Link>
+            <Link to='/wines'>Wines</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+        </div>
+        <div className='rightSide'>
+            <Link to='/'>Home</Link>
+            <Link to='/wines'>Wines</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+            <button>
+              <ReorderIcon/>
+            </button>
+        </div>
     </div>
   )
 }
